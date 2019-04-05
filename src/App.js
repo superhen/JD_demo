a1d6bf07fe139805d94c6cc8ead885e5f8c9164a
Henry <henry@henryz.cn>
2019-04-05 16:10:01 +0800

    [Update]

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Link,
+  Route,
+  Prompt,
+  Redirect,
+  BrowserRouter as Router
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <ul class='list'>
+            <li><Link to="/">首页</Link></li>
+            <li><Link to="/product/0">台灯</Link></li>
+            <li><Link to="/good/1">沐浴露</Link></li>
+            <li><Link to="/product/2">SK2</Link></li>
+          </ul>
+          <hr />
+          <Route path='/' exact render={(Routerprops)=>{
+            return (
+            <div>
+              <button onClick={Routerprops.history.goBack}>返回上级</button>
+              <span>这是首页</span>
+            </div>)
+          }}></Route>
+          <Route path={['/product/:id','/good/:id']} component={Product}></Route>
+        </div>
+      </Router>
     );
   }
 }
-
+class Product extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.history.goBack}>返回上级</button>
+        <span>这是{this.props.match.params.id}的页面</span>
+        <p>
+        <button onClick={()=>this.props.history.push('/',null)}>回到首页</button>
+        </p>
+      </div>
+    )
+  }
+}
 export default App;
